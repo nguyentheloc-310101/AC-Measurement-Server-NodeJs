7487ea3564c616a2ccd4618c37159bf31b1ab4d5
loc-dev <nguyentheloc310101@gmail.com>
2023-11-30 15:28:59 +0700

    fix(api route): remove unused api

--- adafruit-server/gate-way.js
+++ adafruit-server/gate-way.js
@@ -1,62 +1,41 @@
-import mqtt from "mqtt";
+import mqtt from 'mqtt';
 
 const realtimeUpdate = (io) => {
-    const username = `${process.env.ADAFRUIT_IO_USERNAME}`;
-    const key = `${process.env.ADAFRUIT_KEY}`;
-    const host = "mqtt://io.adafruit.com";
-    const client = mqtt.connect(host, {
-        username: username,
-        password: key,
-    });
+  const username = `${process.env.ADAFRUIT_IO_USERNAME}`;
+  const key = `${process.env.ADAFRUIT_KEY}`;
+  const host = 'mqtt://io.adafruit.com';
+  const client = mqtt.connect(host, {
+    username: username,
+    password: key,
+  });
 
-    client.on("connect", () => {
-        console.log("Connected to adafruit");
-        // Subscribe to the temperature feed
-        client.subscribe(`${username}/feeds/yolo-sensor`);
-        // console.log("Subscribe temperature feed");
+  client.on('connect', () => {
+    console.log('Connected to adafruit');
+    client.subscribe(`${username}/feeds/device1`);
 
-        // Subscribe to the humidity feed
-        client.subscribe(`${username}/feeds/dht20-humid`);
-        // console.log("Subscribe humidity feed");
+    client.subscribe(`${username}/feeds/device2`);
 
-        // Subscribe to the fan feed
-        client.subscribe(`${username}/feeds/yolo-fan`);
-        // console.log("Subscribe fan feed");
+    client.subscribe(`${username}/feeds/device3`);
 
+    client.subscribe(`${username}/feeds/device4`);
 
-        // Subscribe to the light feed
-        client.subscribe(`${username}/feeds/yolo-led`);
-        // console.log("Subscribe light feed"); 
+    // client.subscribe(`${username}/feeds/person`);
+  });
 
-        // // Subscribe to the crops status feed
-        // client.subscribe(`${username}/feeds/person`);
-        // // console.log("Subscribe crops status feed");
-    });
+  client.on('message', (topic, message) => {
+    let data = message;
 
-    client.on("message", (topic, message) => {
-        let data = message;
-
-        if (topic.endsWith("yolo-sensor")) {
-            // Emit a "temperatureUpdate" event with the new temperature data
-            io.emit("temperatureUpdate", { temperature: parseInt(data) });
-            console.log(parseInt(data));
-            console.log(`Temperature: ${data}°C`);
-        } else if (topic.endsWith("dht20-humid")) {
-            // Emit a "humidityUpdate" event with the new humidity data
-            io.emit("humidityUpdate", { humidity: parseInt(data) });
-            console.log(parseInt(data));
-            console.log(`Humidity: ${data}%`);
-        } else if (topic.endsWith("yolo-fan")) {
-            // Emit a "fanUpdate" event with the new fan data
-            io.emit("fanUpdate", { fan: parseInt(data) });
-            console.log(`Fan: ${data}`);
-        
-        } else if (topic.endsWith("yolo-led")) {
-            // Emit a "lightUpdate" event with the new light data
-            io.emit("ledUpdate", { led: parseInt(data) });
-            console.log(`Light fron gate-way: ${data}`);
-        }
-    });
+    if (topic.endsWith('devices1')) {
+      //TODO:have to check which is this device
+      // Emit a "fanUpdate" event with the new fan data
+      io.emit('fanUpdate', { fan: parseInt(data) });
+      console.log(`Fan: ${data}`);
+    } else if (topic.endsWith('devices2')) {
+      // Emit a "lightUpdate" event with the new light data
+      io.emit('ledUpdate', { led: parseInt(data) });
+      console.log(`Light from gate-way: ${data}`);
+    }
+  });
 };
 
 export default realtimeUpdate;
--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import * as dataController from '../controllers/dataController.js';
-import {verifyToken} from '../middlewares/authenMiddlewares.js';
+import { verifyToken } from '../middlewares/authenMiddlewares.js';
 
 const router = express();
 
@@ -9,53 +9,17 @@ const router = express();
  * tags:
  *   name: IOT_Data
  *   description: SmartHome devices's data
- * */ 
-
-
+ * */
 
 /**
  * @openapi
  *  /api/data/currentTemperature:
- *      get: 
- *          summary: Return the current temperature
- *          tags: [IOT_Data]
- *          response:
- *              200:
- *                  description: The current temperature
- *                  content:
- *                      application/json:
- *                        schema:
- *                          type: object
- *                          properties:
- *                            data:
- *                              type: object
- *                              properties:
- *                                id:
- *                                  type: integer
- *                                  description: The user ID.
- *                                  example: 0
- *                                name:
- *                                  type: string
- *                                  description: The user's name.
- *                                  example: Leanne Graham
+
  *  
  * */
-router.get("/currentTemperature",   dataController.currentTemperature);
-
-router.get("/currentHumid", dataController.currentHumid);
-
-router.get("/lastFan", dataController.lastFan)
-
-router.get("/lastLed", dataController.lastLed);
-
-router.get("/weektemperatures",  dataController.getDayTemperatures);
-router.get("/weekhumids",  dataController.getDayHumidities);
-router.get("/todayWarnings",  dataController.getWarningsInDay);
-router.get("/tempHumidData", dataController.getDayTempAndHumid);
-
-
-router.post("/setFan", dataController.setFan);
-
-router.post("/toggleLed",  dataController.toggleLed);
+router.get('/lastFan', dataController.lastFan);
+router.get('/lastLed', dataController.lastLed);
+router.post('/setFan', dataController.setFan);
+router.post('/toggleLed', dataController.toggleLed);
 
 export default router;
